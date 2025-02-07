4fb3d770c34d0097e90daa5cc1bd43311e8952cd
Tokyo-dream <1695575018@qq.com>
2025-02-07 22:19:19 +0800

    添加白屏监控和页面停留事件监控

--- src/monitor/blankScreenTracker.ts
+++ src/monitor/blankScreenTracker.ts
@@ -5,3 +5,70 @@
         
     }
 */
+
+import { lazyReport } from "./report";
+
+interface Callback {
+    (): void;
+}
+
+function domOnload(callback: Callback): void {
+    if (document.readyState === "complete") {
+        callback();
+    } else {
+        window.addEventListener("load", callback);
+    }
+}
+
+export function blankScreen(): void {
+    // 需要检测的元素
+    const wrapperElements: string[] = ["html", "body", "#container", ".content"];
+    let emptyPoints: number = 0;
+
+    function getSelector(element: Element): string {
+        if (element.id) {
+            return `#${element.id}`;
+        } else if (element.className) {
+            return "." + (element.className as string)
+                .split(" ")
+                .filter(item => !!item)
+                .join(".");
+        } else {
+            return element.nodeName.toLowerCase();
+        }
+    }
+
+    function isWrapper(element: Element): void {
+        const selector: string = getSelector(element);
+        if (wrapperElements.indexOf(selector) !== -1) {
+            emptyPoints++;
+        }
+    }
+
+    domOnload((): void => {
+        for (let i = 1; i <= 9; i++) {
+            const xElements: Element[] = document.elementsFromPoint(
+                window.innerWidth * i / 10,
+                window.innerHeight / 2
+            );
+            const yElements: Element[] = document.elementsFromPoint(
+                window.innerWidth / 2,
+                window.innerHeight * i / 10
+            );
+            if (xElements.length > 0) {
+                isWrapper(xElements[0]);
+            }
+            if (yElements.length > 0) {
+                isWrapper(yElements[0]);
+            }
+        }
+
+        if (emptyPoints >= 18) {
+            lazyReport("blankScreen", {
+                type: "blankScreen",
+                screen: `${window.screen.width}x${window.screen.height}`,
+                viewPoint: `${window.innerWidth}x${window.innerHeight}`
+            });
+        }
+    });
+}
--- src/monitor/pageTracker.ts
+++ src/monitor/pageTracker.ts
@@ -3,3 +3,28 @@
     上报停留的页面、停留的时长、
 
 */
+import { lazyReport } from "./report";
+
+// 记录页面进入时的时间戳
+const startPageTime: number = Date.now();
+
+export function pageTracker(): void {
+  // 上报页面停留时长的函数
+  function reportDuration(): void {
+    const duration: number = Date.now() - startPageTime;
+    // 使用 lazyReport 上报数据
+    lazyReport("pageDuration", {
+      pageURL: window.location.href,
+      duration: duration
+    });
+  }
+
+  window.addEventListener("beforeunload", reportDuration);
+
+  // 兼容单页应用以及页面在后台时的情况
+  document.addEventListener("visibilitychange", (): void => {
+    if (document.visibilityState === "hidden") {
+      reportDuration();
+    }
+  });
+}
